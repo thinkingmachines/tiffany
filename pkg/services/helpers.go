fc87d46a4ab94e2814db10dc56180f1107677111
Lester James V. Miranda <ljvmiranda@gmail.com>
2019-03-24 00:52:55 +0800

    Add helper for georeferencing

--- pkg/services/helpers.go
+++ pkg/services/helpers.go
@@ -8,7 +8,9 @@ import (
 	"image"
 	"image/png"
 	"log"
+	"math"
 	"os"
+	"strconv"
 
 	"googlemaps.github.io/maps"
 )
@@ -31,6 +33,30 @@ func GetGSMImage(client *maps.Client, coordinate []string, zoom int, size []int)
 	}
 
 	return img
+}
+
+// GeoreferenceImage converts a Static Maps image into a geo-referenced TIFF
+func GeoreferenceImage(coordinate []string, size []int, inpath string, outpath string) {
+
+	// Define projection constants
+	const projector float64 = 156543.03392
+	const maxExtent float64 = 20037508.34
+
+	lat4326 := strconv.ParseFloat(coordinate[0], 64)
+	lon4326 := strconv.ParseFloat(coordinate[1], 64)
+
+	latCenter := size[0] / 2
+	lonCenter := size[1] / 2
+
+	// Compute the GSD Resolution and convert to EPSG3857
+	gsdResolution := projector * math.Cos(lat4326*math.Pi/180) / math.Pow(2, 17)
+	lat3857 := Math.Log((Math.Tan(90+lat4326) * math.Pi / 360)) / (math.Pi / 180) * maxExtent / 180
+	lon3857 := lon4326 * maxExtent / 180
+
+	// Compute boundaries
+	upperLeftY := lat3857 + gsdResolution*latCenter
+	upperLeftX := lon3857 + gsdResolution*lonCenter
+
 }
 
 // SaveImagePNG exports an image into the given image type (PNG or TIFF)

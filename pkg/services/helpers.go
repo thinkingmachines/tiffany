3b76da0cbea58b29cf52670dec9184ed43bb694a
Lj Miranda <12949683+ljvmiranda921@users.noreply.github.com>
2019-03-24 15:44:37 +0800

    Add automated georeferencing feature (#5)
    Resolves #2 
    
    This commit also adds another flag, `--without-reference` to toggle the use of this feature (will only
    output PNG images).
    
    Signed-off-by: Lester James V. Miranda <lj@thinkingmachin.es>

--- pkg/services/helpers.go
+++ pkg/services/helpers.go
@@ -8,8 +8,13 @@ import (
 	"image"
 	"image/png"
 	"log"
+	"math"
 	"os"
+	"path/filepath"
+	"strconv"
 
+	"github.com/disintegration/imaging"
+	"github.com/lukeroth/gdal"
 	"googlemaps.github.io/maps"
 )
 
@@ -21,7 +26,7 @@ func GetGSMImage(client *maps.Client, coordinate []string, zoom int, size []int)
 		Center:  fmt.Sprintf("%s,%s", coordinate[0], coordinate[1]),
 		Zoom:    zoom,
 		Size:    fmt.Sprintf("%dx%d", size[0], size[1]),
-		Scale:   2,
+		Scale:   1,
 		MapType: "satellite",
 	}
 	// Perform request
@@ -33,18 +38,66 @@ func GetGSMImage(client *maps.Client, coordinate []string, zoom int, size []int)
 	return img
 }
 
-// SaveImagePNG exports an image into the given image type (PNG or TIFF)
-func SaveImagePNG(img image.Image, path string, fname string) {
-	log.Printf("Saving image to %s", path)
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		os.MkdirAll(path, os.ModePerm)
+// GeoreferenceImage converts a Static Maps image into a geo-referenced TIFF
+func GeoreferenceImage(coordinate []string, size []int, zoom int, inpath string, outpath string) {
+
+	log.Println("Georeferencing image into %s", outpath)
+	// Define projection constants
+	const projector float64 = 156543.03392
+	const maxExtent float64 = 20037508.34
+
+	if _, err := os.Stat(filepath.Dir(outpath)); os.IsNotExist(err) {
+		os.MkdirAll(filepath.Dir(outpath), os.ModePerm)
 	}
 
-	f, err := os.Create(fmt.Sprintf("%s%s", path, fname))
+	lat4326, _ := strconv.ParseFloat(coordinate[0], 64)
+	lon4326, _ := strconv.ParseFloat(coordinate[1], 64)
+
+	latCenter := size[0] / 2
+	lonCenter := size[1] / 2
+
+	// Compute the GSD Resolution and convert to EPSG3857
+	gsdResolution := projector * math.Cos(lat4326*math.Pi/180) / math.Pow(2, float64(zoom))
+	lat3857 := (math.Log(math.Tan((90+lat4326)*math.Pi/360)) / (math.Pi / 180)) * maxExtent / 180
+	lon3857 := lon4326 * maxExtent / 180
+
+	// Compute boundaries
+	upperLeftY := lat3857 + gsdResolution*float64(latCenter)
+	upperLeftX := lon3857 - gsdResolution*float64(lonCenter)
+
+	// Read source image and its driver
+	srcDataset, _ := gdal.Open(inpath, gdal.ReadOnly)
+	driver, _ := gdal.GetDriverByName("GTiff")
+
+	// Open destination dataset
+	dstDataset := driver.CreateCopy(outpath, srcDataset, 0, nil, nil, nil)
+	dstDataset.SetGeoTransform([6]float64{upperLeftX, gsdResolution, 0, upperLeftY, 0, -gsdResolution})
+
+	// Get raster projection
+	srs := gdal.CreateSpatialReference("")
+	srs.FromEPSG(3857)
+	destWKT, _ := srs.ToWKT()
+
+	// Create destination dataset
+	dstDataset.SetProjection(destWKT)
+
+	defer dstDataset.Close()
+	defer srcDataset.Close()
+}
+
+// SaveImagePNG exports an image into a file
+func SaveImagePNG(img image.Image, path string) {
+	log.Printf("Saving image to %s", path)
+	if _, err := os.Stat(filepath.Dir(path)); os.IsNotExist(err) {
+		os.MkdirAll(filepath.Dir(path), os.ModePerm)
+	}
+
+	f, err := os.Create(fmt.Sprintf("%s", path))
 	if err != nil {
 		log.Panic(err)
 	}
+	imgRGBA := imaging.Clone(img)
 
 	defer f.Close()
-	png.Encode(f, img)
+	png.Encode(f, imgRGBA)
 }
--- pkg/services/pipelines.go
+++ pkg/services/pipelines.go
@@ -4,15 +4,26 @@ package services
 
 import (
 	"fmt"
+	"path/filepath"
 
 	"github.com/thinkingmachines/tiffany/pkg/auth"
 )
 
 // RunPipeline executes the whole download and georeference tasks for a single coordinate
-func RunPipeline(coordinate []string, zoom int, size []int, path string) {
+func RunPipeline(coordinate []string, zoom int, size []int, path string, noRef bool) {
+
+	const gsmSubDir string = "png"
+	const geoSubDir string = "tif"
+
+	// Create filenames for output artifacts
+	fnameFormat := fmt.Sprintf("%s-%s-%d-%dx%d", coordinate[0], coordinate[1], zoom, size[0], size[1])
+	pngPath := filepath.Join(path, gsmSubDir, fnameFormat+".png")
+	tifPath := filepath.Join(path, geoSubDir, fnameFormat+".tiff")
+
 	client := auth.GetStaticMapsClient()
 	gsmImage := GetGSMImage(client, coordinate, zoom, size)
-	pngFileName := fmt.Sprintf("%s-%s-%d-%dx%d.png", coordinate[0], coordinate[1], zoom, size[0], size[1])
-	SaveImagePNG(gsmImage, fmt.Sprintf("%spng/", path), pngFileName)
-
+	SaveImagePNG(gsmImage, pngPath)
+	if !noRef {
+		GeoreferenceImage(coordinate, size, zoom, pngPath, tifPath)
+	}
 }

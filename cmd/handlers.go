b1a8f85a958b8f66fffeca165345a8ab68295408
Lester James V. Miranda <ljvmiranda@gmail.com>
2019-03-24 22:19:47 +0800

    Add handlers for clipping the ShapeFile

--- cmd/handlers.go
+++ cmd/handlers.go
@@ -17,10 +17,53 @@ import (
 	"googlemaps.github.io/maps"
 )
 
+// ClipLabelbyExtent gets the extent of an input raster and clips a shapefile from it
+func ClipLabelbyExtent(extent gdal.Geometry, shpFile gdal.Layer, outpath string) {
+	if _, err := os.Stat(filepath.Dir(outpath)); os.IsNotExist(err) {
+		os.MkdirAll(filepath.Dir(outpath), os.ModePerm)
+	}
+
+	// Perform filtering of labels
+	shpFile.SetSpatialFilter(extent)
+	shpFile.ResetReading()
+
+	outDriver := gdal.OGRDriverByName("GeoJSON")
+	outDataSource, _ := outDriver.Create(outpath, []string{})
+	outDataSource.CopyLayer(shpFile, "Labels", []string{})
+	outDataSource.Destroy()
+}
+
+// GetRasterExtent computes for the extent of a TIFF image and returns a Geometry
+func GetRasterExtent(tifPath string) gdal.Geometry {
+	src, err := gdal.Open(tifPath, gdal.ReadOnly)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// affine (transformation) is organized as
+	// (ulx, xres, xskew, uly, yskew, yres)
+	affine := src.GeoTransform()
+	lrx := affine[0] + float64(src.RasterXSize())*affine[1]
+	lry := affine[3] + float64(src.RasterYSize())*affine[5]
+	defer src.Close()
+
+	// Convert extents into a WKT representation
+	ring := gdal.Create(gdal.GT_LinearRing)
+	ring.AddPoint2D(affine[0], affine[3]) // top-left
+	ring.AddPoint2D(lrx, affine[3])       // top-right
+	ring.AddPoint2D(lrx, lry)             // bottom-right
+	ring.AddPoint2D(affine[0], lry)       // bottom-left
+	ring.AddPoint2D(affine[0], affine[3]) // top-left
+
+	// Create a polygon by closing the ring
+	extent := gdal.Create(gdal.GT_Polygon)
+	extent.AddGeometryDirectly(ring)
+	return extent
+}
+
 // GeoreferenceImage converts a Static Maps image into a geo-referenced TIFF
 func GeoreferenceImage(coordinate []string, size []int, zoom int, inpath string, outpath string) {
 
-	log.Printf("Georeferencing image into %s", outpath)
 	// Define projection constants
 	const projector float64 = 156543.03392
 	const maxExtent float64 = 20037508.34
@@ -115,36 +158,56 @@ func ReprojectImage(path string) {
 	defer out.Close()
 }
 
+// ReadShapeFile opens a SHP file and returns a Layer of Features
+func ReadShapeFile(lblPath string) gdal.Layer {
+	srs := gdal.CreateSpatialReference("")
+	srs.FromEPSG(4326)
+	lblDataSource := gdal.OpenDataSource(lblPath, 1)
+	// lblLayer := lblDataSource.CreateLayer("Labels", srs, gdal.GT_MultiPolygon, []string{})
+	lblLayer := lblDataSource.LayerByIndex(0)
+	return lblLayer
+
+}
+
 // RunPipeline executes the whole download and georeference tasks for a single coordinate
-func RunPipeline(coordinate []string, zoom int, size []int, path string, noRef bool) {
+func RunPipeline(coordinate []string, zoom int, size []int, path string, noRef bool, wtLbl string) {
 
 	const gsmSubDir string = "png"
 	const geoSubDir string = "tif"
+	const lblSubDir string = "json"
 
 	// Create filenames for output artifacts
 	fnameFormat := fmt.Sprintf("%s_%s_%d_%dx%d", coordinate[0], coordinate[1], zoom, size[0], size[1])
 	pngPath := filepath.Join(path, gsmSubDir, fnameFormat+".png")
 	tifPath := filepath.Join(path, geoSubDir, fnameFormat+".tiff")
+	lblPath := filepath.Join(path, lblSubDir, fnameFormat+".geojson")
 
 	client := GetStaticMapsClient()
 	gsmImage := GetGSMImage(client, coordinate, zoom, size)
+	log.Printf("Saving image to %s", pngPath)
 	SaveImagePNG(gsmImage, pngPath)
 	if !noRef {
+		log.Printf("Georeferencing image into %s", tifPath)
 		GeoreferenceImage(coordinate, size, zoom, pngPath, tifPath)
 		ReprojectImage(tifPath)
+	}
+	if len(wtLbl) > 0 {
+		log.Printf("Saving labels to %s", lblPath)
+		extent := GetRasterExtent(tifPath)
+		shpFile := ReadShapeFile(wtLbl)
+		ClipLabelbyExtent(extent, shpFile, lblPath)
 	}
 }
 
 // SaveImagePNG exports an image into a file
-func SaveImagePNG(img image.Image, path string) {
-	log.Printf("Saving image to %s", path)
-	if _, err := os.Stat(filepath.Dir(path)); os.IsNotExist(err) {
-		os.MkdirAll(filepath.Dir(path), os.ModePerm)
+func SaveImagePNG(img image.Image, outpath string) {
+	if _, err := os.Stat(filepath.Dir(outpath)); os.IsNotExist(err) {
+		os.MkdirAll(filepath.Dir(outpath), os.ModePerm)
 	}
 
-	f, err := os.Create(fmt.Sprintf("%s", path))
+	f, err := os.Create(fmt.Sprintf("%s", outpath))
 	if err != nil {
-		log.Panic(err)
+		log.Fatal(err)
 	}
 	imgRGBA := imaging.Clone(img)
 

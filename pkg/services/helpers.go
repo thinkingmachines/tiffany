6ec283404e018ac9c3a94db5822da4b95224dbcb
Lester James V. Miranda <ljvmiranda@gmail.com>
2019-03-24 12:21:57 +0800

    Update helpers for gdal

--- pkg/services/helpers.go
+++ pkg/services/helpers.go
@@ -12,6 +12,7 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/lukeroth/gdal"
 	"googlemaps.github.io/maps"
 )
 
@@ -39,24 +40,53 @@ func GetGSMImage(client *maps.Client, coordinate []string, zoom int, size []int)
 func GeoreferenceImage(coordinate []string, size []int, inpath string, outpath string) {
 
 	// Define projection constants
+	const epsg int = 3857
 	const projector float64 = 156543.03392
 	const maxExtent float64 = 20037508.34
 
-	lat4326 := strconv.ParseFloat(coordinate[0], 64)
-	lon4326 := strconv.ParseFloat(coordinate[1], 64)
+	lat4326, err := strconv.ParseFloat(coordinate[0], 64)
+	if err != nil {
+		log.Panic(err)
+	}
+	lon4326, err := strconv.ParseFloat(coordinate[1], 64)
+	if err != nil {
+		log.Panic(err)
+	}
 
 	latCenter := size[0] / 2
 	lonCenter := size[1] / 2
 
 	// Compute the GSD Resolution and convert to EPSG3857
 	gsdResolution := projector * math.Cos(lat4326*math.Pi/180) / math.Pow(2, 17)
-	lat3857 := Math.Log((Math.Tan(90+lat4326) * math.Pi / 360)) / (math.Pi / 180) * maxExtent / 180
+	lat3857 := math.Log((math.Tan(90+lat4326) * math.Pi / 360)) / (math.Pi / 180) * maxExtent / 180
 	lon3857 := lon4326 * maxExtent / 180
 
 	// Compute boundaries
-	upperLeftY := lat3857 + gsdResolution*latCenter
-	upperLeftX := lon3857 + gsdResolution*lonCenter
+	upperLeftY := lat3857 + gsdResolution*float64(latCenter)
+	upperLeftX := lon3857 - gsdResolution*float64(lonCenter)
 
+	// Specify raster location through the geotransform array
+	gt := [6]float64{upperLeftX, gsdResolution, 0, upperLeftY, 0, -gsdResolution}
+
+	// Read source image and its driver
+	srcDataset, err := gdal.Open(inpath, gdal.ReadOnly)
+	if err != nil {
+		log.Panic(err)
+	}
+	driver, err := gdal.GetDriverByName("GTiff")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// Create copy for destination image and update it
+	dstDataset := driver.CreateCopy(outpath, srcDataset, 0, nil, nil, nil)
+	dstDataset.SetGeoTransform(gt)
+
+	// Get raster projection
+	spatialRef := gdal.CreateSpatialReference("")
+	spatialRef.FromEPSG(epsg)
+	srString, err := spatialRef.ToWKT()
+	dstDataset.SetProjection(srString)
 }
 
 // SaveImagePNG exports an image into the given image type (PNG or TIFF)

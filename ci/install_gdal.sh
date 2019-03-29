fd2d0890cd92161378671463349dcbc70cfe4c66
Lester James V. Miranda <ljvmiranda@gmail.com>
2019-03-29 11:20:53 +0800

    [WIP] Downgrade GDAL

--- ci/install_gdal.sh
+++ ci/install_gdal.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 apt-get update -y && apt install -y software-properties-common
-add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
-add-apt-repository -y ppa:nextgis/ppa
+add-apt-repository -y ppa:ubuntugis/ppa && apt-get update
+# add-apt-repository -y ppa:nextgis/ppa
 apt-get update && apt-get install -y \
     gdal-bin \
-    libgdal-dev \
-    libgdal20
+    libgdal-dev
 
 export CPLUS_INCLUDE_PATH=/usr/include/gdal
 export C_INCLUDE_PATH=/usr/include/gdal

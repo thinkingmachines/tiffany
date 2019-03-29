135b621246148e60756ac494a49a08732fa3f3a9
Lester James V. Miranda <ljvmiranda@gmail.com>
2019-03-29 11:30:23 +0800

    [WIP] Add gdalpc install script

--- ci/install_gdal.sh
+++ ci/install_gdal.sh
@@ -2,10 +2,18 @@
 
 apt-get update -y && apt install -y software-properties-common
 add-apt-repository -y ppa:ubuntugis/ppa && apt-get update
-# add-apt-repository -y ppa:nextgis/ppa
 apt-get update && apt-get install -y \
     gdal-bin \
     libgdal-dev
 
 export CPLUS_INCLUDE_PATH=/usr/include/gdal
 export C_INCLUDE_PATH=/usr/include/gdal
+
+# Add gdal.pc in config
+which gdal-config || exit ${LINENO}
+cd /usr/lib/pkgconfig || exit ${LINENO}
+[ -r gdal.pc ] && exit ${LINENO}
+curl -ks 'https://gist.githubusercontent.com/nicerobot/5160658/raw/gdal.pc.template' \
+| sed -e 's/^/echo /' \
+| bash - \
+> ${1:-gdal.pc}

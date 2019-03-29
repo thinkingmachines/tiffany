9eadc7ccf25d5b08465ebbdc218eba18a6e9947e
Lester James V. Miranda <ljvmiranda@gmail.com>
2019-03-29 10:38:36 +0800

    [WIP] Update install script

--- ci/install_gdal.sh
+++ ci/install_gdal.sh
@@ -2,6 +2,7 @@
 
 apt-get update -y && apt install -y software-properties-common
 add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
+add-apt-repository -y ppa:nextgis/ppa
 apt-get update && apt-get install -y \
     gdal-bin \
     libgdal-dev \

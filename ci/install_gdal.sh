63484677607ac565d97c5eec76543375f2b0c9dc
Lester James V. Miranda <ljvmiranda@gmail.com>
2019-03-29 10:31:25 +0800

    [WIP] Use install script

---
+++ ci/install_gdal.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt-get update -y && apt install -y software-properties-common
+add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
+apt-get update && apt-get install -y \
+    gdal-bin \
+    libgdal-dev \
+    libgdal20
+
+export CPLUS_INCLUDE_PATH=/usr/include/gdal
+export C_INCLUDE_PATH=/usr/include/gdal
